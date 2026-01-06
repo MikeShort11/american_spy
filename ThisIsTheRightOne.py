eab1d0a70b1f615200995c558c66d3f0458f743e
Michael Short <michael.short094@gmail.com>
2026-01-05 22:48:51 -0700

    THIS BETTER WORK THIS TIME

---
+++ ThisIsTheRightOne.py
@@ -0,0 +1,25 @@
+# Simple Caesar Cipher Encryption without if statements
+
+def encrypt(text, shift):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    #how can we sotre the result
+    
+    #Time to learn aout Loops!
+        #where is the character we start at?
+        #how do we shift it
+        #How can we get the new character
+        #How do we add it to the result
+
+    return result#NOTE FOR MICHAEL - EXPLAIN THIS LINE
+
+# Main program
+def main():
+    print("Welcome to Caesar Cipher Encryption!")
+    #message = TODO:new_KEYWORD!("Enter a message to encrypt (lowercase letters only, no spaces): ")
+    #shift = int(TODO:new_KEYWORD AGAIN!("Enter a shift number: "))
+
+    encrypted = encrypt(message, shift)#NOTE FOR MICHAEL - EXPLAIN THIS LINE
+    print("Encrypted message:", encrypted)
+
+if __name__ == "main":
+    main()
