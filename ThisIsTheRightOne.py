9f59cc242c8d77f14c7c5445741feccd3fd7e577
Michael Short <153890342+MikeShort11@users.noreply.github.com>
2026-01-09 10:49:47 -0700

    Implement encryption and decryption functions

--- ThisIsTheRightOne.py
+++ ThisIsTheRightOne.py
@@ -1,25 +1,44 @@
 # Simple Caesar Cipher Encryption without if statements
 
-#def encrypt(HowDoWeSetTheMessage?):
+def encrypt(message, shift):
     alphabet = "abcdefghijklmnopqrstuvwxyz"
-    #how can we sotre the result
+    result = ""
     
-    #Time to learn aout Loops!
-        #where is the character we start at?
-        #how do we shift it
-        #How can we get the new character
-        #How do we add it to the result
+    for letter in message:
+        start_index = alphabet.index(letter)
+        new_index = start_index + shift
+        new_letter = alphabet[new_index % 26]
+        result = result + new_letter
 
-    return result#NOTE FOR MICHAEL - EXPLAIN THIS LINE
+    return result
+#comments dont affect the code
+
+def decrypt(message, shift):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    result = ""
+    
+    for letter in message:
+        start_index = alphabet.index(letter)
+        new_index = start_index - shift
+        new_letter = alphabet[new_index % 26]
+        result = result + new_letter
+
+    return result 
 
 # Main program
 def main():
     print("Welcome to Caesar Cipher Encryption!")
-    #message = TODO:new_KEYWORD!("Enter a message to encrypt (lowercase letters only, no spaces): ")
-    #shift = int(TODO:new_KEYWORD AGAIN!("Enter a shift number: "))
+    user_selection = input("Press 1 to encrypt, press 2 to decrypt")
+    if user_selection == "2":
+        message = input("Enter a message to encrypt (lowercase letters only, no spaces): ")
+        shift = int(input("Enter a shift number: "))
+        encrypted = encrypt(message, shift)
+        print("Encrypted message:", encrypted)
+    if user_selection == "2":
+        message = input("Enter a message to decrypt (lowercase letters only, no spaces): ")
+        shift = int(input("Enter a shift number: "))
+        decrypted = decrypt(message, shift)
+        print("decrypted message:", decrypted)      
 
-    encrypted = encrypt(message, shift)#NOTE FOR MICHAEL - EXPLAIN THIS LINE
-    print("Encrypted message:", encrypted)
-
-if __name__ == "main":
+if __name__ == "__main__":
     main()
